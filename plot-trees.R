c971c86d5c496eb07b3993f08674205b8f0177eb
Matt Dickenson <mcdickenson@gmail.com>
2014-03-18 15:56:11 -0400

    prep for presentation

--- plot-trees.R
+++ plot-trees.R
@@ -1,3 +1,6 @@
+# set up
+setwd('~/desktop/icews-dtree')
+library(tikzDevice)
 
 # load trees
 load("data.rda")
@@ -11,6 +14,7 @@ load("tree_mine_pruned.rda")
 
 summary(tree_mine)
 prp(tree_mine)
+prp(tree_mine_pruned)
 
 # out-of-sample performance
 
@@ -40,13 +44,7 @@ splitlabs = function(x, labs, digits, varlen, faclen){
   return(tmp)
 }
 nodelabs = function(x, labs, digits, varlen){
-  # tmp = gsub("0.", ".", labs, fixed=TRUE)
   tmp = sub("0.", ".", labs, fixed=TRUE)
-  # tmp = sub(c("0", "1"), c("Peace", "War"), tmp, fixed=TRUE)
-  # tmp = sub("1", "War", tmp, fixed=TRUE)
-  # tmp = sub(".War", ".1", tmp, fixed=TRUE)
-  # tmp = sub(".0War", ".01", tmp, fixed=TRUE)
-  # tmp = sub("1", "War", tmp)
   return(tmp)
 }
 
@@ -64,15 +62,8 @@ legend("topright", pch=16, col=c(green, red),
   legend=c("Peace", "War"),
   inset=.1)
 
-yhat_test = predict(tree_mine_pruned, type='class', newdata=test)
-yobs_test = test$hostile4
-sum(as.numeric(yhat_test!=yobs_test))/sum(yobs_test) # 1.008
 
-
-pathGraphics = "~/github/cs571/graphics"
-setwd(pathGraphics)
-
-tikz('tree.tex', standAlone = FALSE, width=4, height=6)
+pdf('tree-icews.pdf', width=4, height=6)
 prp(tree_mine_pruned, extra=7,
   node.fun=nodelabs, 
   split.fun=splitlabs,
@@ -85,92 +76,5 @@ legend("topright", pch=16, col=c(green, red),
 dev.off()
 
 
-#### compare tree to other models
-mse = function(est, obs){
-  err = obs - est
-  sq.err = err^2
-  mean.sq.err = mean(sq.err, na.rm=TRUE)
-  return(mean.sq.err)
-}
 
-# misclass = function(est, obs){
-#   sum(as.numeric(est!=obs))/length(obs)
-# }
-
-precision = function(est, obs){
-  tp = length(which(est==1 & obs==1))
-  fp = length(which(est==1 & obs==0))
-  (tp/(tp+fp+1e-9))
-}
-
-recall = function(est, obs){
-  tp = length(which(est==1 & obs==1))
-  fn = length(which(est==0 & obs==1))
-  (tp/(tp+fn))
-}
-## training set
-yobs = train$hostile4
-# null model (all zeroes)
-zeroes = rep(0, length(yobs))
-mse(zeroes, yobs)
-precision(zeroes, yobs)
-recall(zeroes, yobs)
-
-# glm
-form_mine = as.formula(hostile4 ~ actorMIL + 
-  quad1p.l1 + quad2p.l1 + quad3p.l1 + quad4p.l1 +
-  event1.d1 + event2.d1 + event3.d1 + event4.d1 + event5.d1 + 
-  event6.d1 + event7.d1 + event8.d1 + event9.d1 + event10.d1 +
-  event11.d1+ event12.d1+ event13.d1+ event14.d1+ event15.d1 +
-  event16.d1+ event17.d1+ event18.d1+ event19.d1+ event20.d1 )
-glmfit = glm(form_mine, data=train, family=binomial(link=logit))
-summary(glmfit)
-
-probpred = predict(glmfit, type='response')
-ypred = ifelse(probpred>0.05, 1, 0)
-mse(ypred, yobs)
-mse(probpred, yobs)
-precision(ypred, yobs)
-recall(ypred, yobs)
-
-# tree
-yhat = as.numeric(predict(tree_mine, type='class'))-1
-
-mse(yhat, yobs)
-precision(yhat, yobs)
-recall(yhat, yobs)
-
-# length(which(yhat==0 & yobs==1))
-# sum(as.numeric(yhat!=yobs)) 
-# sum(yobs)
-# sum(as.numeric(yhat!=yobs))/sum(yobs) # 0.889
-
-
-## test set
-yobs = test$hostile4
-
-# null model
-zeroes = rep(0, length(yobs))
-
-mse(zeroes, yobs)
-precision(zeroes, yobs)
-recall(zeroes, yobs)
-
-# glm
-probpred = predict(glmfit, type='response', newdata=test)
-ypred = ifelse(probpred>0.05, 1, 0)
-mse(ypred, yobs)
-mse(probpred, yobs)
-precision(ypred, yobs)
-recall(ypred, yobs)
-
-# tree
-yhat = as.numeric(predict(tree_mine_pruned, type='class', newdata=test))-1
-summary(yhat)
-mse(yhat, yobs)
-precision(yhat, yobs)
-recall(yhat, yobs)
-
-nrow(train)
-nrow(test)
 
