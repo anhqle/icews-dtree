215eb0f4ed953150c992c0f6c447b31fc239e8cd
Matt Dickenson <mcdickenson@gmail.com>
2014-03-18 13:57:04 -0400

    save trees for plotting

--- build-trees.R
+++ build-trees.R
@@ -8,22 +8,23 @@ getwd()
 full_data = read.csv("merged.csv", as.is=TRUE)
 dim(full_data)
 names(full_data)
+
+# compute helper variables
+names(full_data)
+full_data$hostlevA = ifelse(is.na(full_data$hostlevA), 0, full_data$hostlevA)
+full_data$hostlevB = ifelse(is.na(full_data$hostlevB), 0, full_data$hostlevB)
+summary(full_data$hostlevA)
+summary(full_data$hostlevB)
+full_data$hostile4 = 0
+full_data$hostile4[full_data$hostlevA>=4] = 1
+full_data$hostile4[full_data$hostlevB>=4] = 1
+summary(full_data$hostile4)
+
+# split into training and test sets
 data = full_data[full_data$year<=2005,]
 test = full_data[full_data$year> 2005,]
 dim(data)
-
-# compute helper variables
-names(data)
-data$hostile4
-data$hostlevA = ifelse(is.na(data$hostlevA), 0, data$hostlevA)
-data$hostlevB = ifelse(is.na(data$hostlevB), 0, data$hostlevB)
-summary(data$hostlevA)
-summary(data$hostlevB)
-data$hostile4 = 0
-data$hostile4[data$hostlevA>=4] = 1
-data$hostile4[data$hostlevB>=4] = 1
-summary(data$hostile4)
-
+dim(test)
 
 # modeling
 
@@ -50,6 +51,7 @@ form_mine = as.formula(hostile4 ~
   event20
 )
 
+# build trees 
 ctrl = rpart.control(cp=1e-4)
 start = Sys.time()
 tree_mine = rpart(form_mine, data=data, 
@@ -58,16 +60,20 @@ tree_mine = rpart(form_mine, data=data,
 runtime = Sys.time() - start
 runtime
 
-
-
-
-
-# in-sample performance
 tree_mine
 summary(tree_mine)
 printcp(tree_mine)
 prp(tree_mine)
 
+
+tree_mine_pruned = prune(tree_mine, cp=printcp(tree_mine)[2] )
+prp(tree_mine_pruned)
+
+save(tree_mine, file="tree_mine.rda")
+save(tree_mine_pruned, file="tree_mine_pruned.rda")
+
+
+# in-sample performance
 yhat = predict(tree_mine, type='class')
 yobs = data$hostile4[as.numeric(names(yhat))]
 yobs = ifelse(is.na(yobs), 0, yobs)
@@ -82,22 +88,20 @@ length(yobs)
 length(which(yhat==0 & yobs==1))
 sum(as.numeric(yhat!=yobs)) 
 sum(yobs)
-sum(as.numeric(yhat!=yobs))/sum(yobs) # 0.889
+sum(as.numeric(yhat!=yobs))/sum(yobs) # 1.012
 
 
 yhat = predict(tree_mine_pruned, type='class')
-sum(as.numeric(yhat!=yobs))/sum(yobs) # 0.983
+yhat = as.numeric(yhat) - 1
+head(yhat)
+summary(yhat)
+sum(as.numeric(yhat!=yobs))/sum(yobs) # 1.009
 
 
 # out-of-sample performance
 
-# prune to minimize test error 
-tree_mine_pruned = prune(tree_mine, cp=printcp(tree_mine)[3] )
-printcp(tree_mine_pruned)
-prp(tree_mine_pruned)
-
-yhat_test = predict(tree_mine_pruned, type='class', newdata=test)
+yhat_test = as.numeric(predict(tree_mine_pruned, type='class', newdata=test))-1
 yobs_test = test$hostile4
-sum(as.numeric(yhat_test!=yobs_test))/sum(yobs_test) # 1.008
+sum(as.numeric(yhat_test!=yobs_test))/sum(yobs_test) # 1.31
 
 # see plot-trees.R, etc 
